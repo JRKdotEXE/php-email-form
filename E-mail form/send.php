9d0ccd4113509b3ffa8b8b8fe4a7120fd67ae54c
JRKdotEXE <jirkanovak0505@gmail.com>
2024-02-08 13:29:48 +0100

    Email form

---
+++ E-mail form/send.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+$to = ""; #Destination
+$header = "Form $email";
+
+if (mail($to, $subject, $message, $header)) {
+    echo "Email sent";
+    header("Location:emailsent.html");
+}  else {
+    echo "Sending failed";
+} 
+?>
+
+
